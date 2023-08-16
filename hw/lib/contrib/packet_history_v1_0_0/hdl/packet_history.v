80fe5bccc194f3de3fbe91b6bdeb19452f630566
Gianni Antichi <antichi.gianni@gmail.com>
2023-08-16 13:48:57 +0100

    first version of packet history module

---
+++ hw/lib/contrib/packet_history_v1_0_0/hdl/packet_history.v
@@ -0,0 +1,342 @@
+//-
+// Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+//                          Junior University
+// Copyright (C) 2010, 2011 Muhammad Shahbaz
+// Copyright (C) 2015 Gianni Antichi, Noa Zilberman, Salvator Galea
+// All rights reserved.
+//
+// This software was developed by
+// Stanford University and the University of Cambridge Computer Laboratory
+// under National Science Foundation under Grant No. CNS-0855268,
+// the University of Cambridge Computer Laboratory under EPSRC INTERNET Project EP/H040536/1 and
+// by the University of Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-11-C-0249 ("MRC2"), 
+// as part of the DARPA MRC research programme.
+//
+// @NETFPGA_LICENSE_HEADER_START@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @NETFPGA_LICENSE_HEADER_END@
+//
+
+module packet_history
+#(
+   // -- Master AXI Stream Data Width
+   parameter C_M_AXIS_DATA_WIDTH	   = 512,
+   parameter C_S_AXIS_DATA_WIDTH	   = 512,
+   parameter C_M_AXIS_TUSER_WIDTH	= 128,
+   parameter C_S_AXIS_TUSER_WIDTH	= 128
+   
+)
+(
+   // -- Global Ports
+   input                                      axis_aclk,
+   input                                      axis_resetn,
+
+   // -- Master Stream Ports (interface to data path)
+   output reg [C_M_AXIS_DATA_WIDTH - 1:0]          m_axis_tdata,
+   output reg [((C_M_AXIS_DATA_WIDTH / 8)) - 1:0]  m_axis_tkeep,
+   output reg [C_M_AXIS_TUSER_WIDTH-1:0]           m_axis_tuser,
+   output reg                                      m_axis_tvalid,
+   input                                           m_axis_tready,
+   output reg                                      m_axis_tlast,
+
+   // -- Slave Stream Ports (interface to RX queues)
+   input [C_S_AXIS_DATA_WIDTH - 1:0]          s_axis_tdata,
+   input [((C_S_AXIS_DATA_WIDTH / 8)) - 1:0]  s_axis_tkeep,
+   input [C_S_AXIS_TUSER_WIDTH-1:0]           s_axis_tuser,
+   input                                      s_axis_tvalid,
+   output                                     s_axis_tready,
+   input                                      s_axis_tlast
+   
+);
+
+   function integer log2;
+      input integer number;
+      begin
+         log2=0;
+         while(2**log2<number) begin
+            log2=log2+1;
+         end
+      end
+   endfunction // log2
+
+  //--------------------- Internal Parameter-------------------------
+
+  localparam NUM_QUEUES          = 8;
+  localparam NUM_QUEUES_WIDTH    = log2(NUM_QUEUES);
+  localparam NUM_CORES_HISTORY   = 16;
+  localparam NUM_CORES_BITS      = log2(NUM_CORES_HISTORY);
+  localparam TUPLE_WIDTH         = 112; // 32 (SRC_IP) + 32 (DST_IP) + 16 (SRC_L4_PORT) + 16 (DST_L4_PORT) + 16 (PKT_LENGTH) 
+
+  localparam SRC_PORT_POS        = 16;
+  localparam DST_PORT_POS        = 24;
+
+  localparam NUM_STATES          = 5;
+  localparam FIRST_BATCH         = 1;
+  localparam SECOND_BATCH        = 2;
+  localparam THIRD_BATCH         = 4;
+  localparam FOURTH_BATCH        = 8;
+  localparam SEND_PACKET         = 16;
+
+  integer	i; // mem initialization
+
+  // -- Signals
+  wire [TUPLE_WIDTH-1:0]   tuple;
+  wire                     tuple_valid;
+  reg [NUM_STATES-1:0]     state,state_next;
+
+  // packet fifo signals
+  wire                              in_fifo_nearly_full;     
+  reg                               in_fifo_rd_en;
+  wire                              in_fifo_empty;
+  wire [C_M_AXIS_DATA_WIDTH-1:0]    in_fifo_tdata;
+  wire [C_M_AXIS_TUSER_WIDTH-1:0]   in_fifo_tuser;
+  wire [C_M_AXIS_DATA_WIDTH/8-1:0]  in_fifo_tkeep;
+  wire                              in_fifo_tlast;    
+
+  // tuple fifo signals
+  wire                     tuple_fifo_nearly_full;
+  reg                      tuple_fifo_rd_en;
+  wire                     tuple_fifo_empty;
+  wire [TUPLE_WIDTH-1:0]   tuple_fifo_out;
+
+  // variables related to the internal memory
+  reg [TUPLE_WIDTH-1:0]    mem_history[NUM_CORES_BITS-1:0];
+  reg [NUM_CORES_BITS-1:0] ptr_pos, ptr_pos_next;
+  reg wr_mem_en, wr_mem_en_next;
+
+   /* The size of this fifo has to be large enough to fit the previous modules' headers
+    * and the ethernet header */
+   xpm_fifo_sync #(
+      .FIFO_MEMORY_TYPE     ("auto"),
+      .ECC_MODE             ("no_ecc"),
+      .FIFO_WRITE_DEPTH     (16),
+      .WRITE_DATA_WIDTH     (C_M_AXIS_DATA_WIDTH+C_M_AXIS_TUSER_WIDTH+C_M_AXIS_DATA_WIDTH/8+1),
+      .WR_DATA_COUNT_WIDTH  (1),
+      //.PROG_FULL_THRESH     (PROG_FULL_THRESH),
+      .FULL_RESET_VALUE     (0),
+      .USE_ADV_FEATURES     ("0707"),
+      .READ_MODE            ("fwft"),
+      .FIFO_READ_LATENCY    (1),
+      .READ_DATA_WIDTH      (C_M_AXIS_DATA_WIDTH+C_M_AXIS_TUSER_WIDTH+C_M_AXIS_DATA_WIDTH/8+1),
+      .RD_DATA_COUNT_WIDTH  (1),
+      .PROG_EMPTY_THRESH    (10),
+      .DOUT_RESET_VALUE     ("0"),
+      .WAKEUP_TIME          (0)
+   ) input_fifo (
+      // Common module ports
+      .sleep           (),
+      .rst             (~axis_resetn),
+
+      // Write Domain ports
+      .wr_clk          (axis_aclk),
+      .wr_en           (s_axis_tvalid & s_axis_tready),
+      .din             ({s_axis_tlast, s_axis_tuser, s_axis_tkeep, s_axis_tdata}),
+      .full            (),
+      .prog_full       (in_fifo_nearly_full),
+      .wr_data_count   (),
+      .overflow        (),
+      .wr_rst_busy     (),
+      .almost_full     (),
+      .wr_ack          (),
+
+      // Read Domain ports
+      .rd_en           (in_fifo_rd_en),
+      .dout            ({in_fifo_tlast, in_fifo_tuser, in_fifo_tkeep, in_fifo_tdata}),
+      .empty           (in_fifo_empty),
+      .prog_empty      (),
+      .rd_data_count   (),
+      .underflow       (),
+      .rd_rst_busy     (),
+      .almost_empty    (),
+      .data_valid      (),
+
+      // ECC Related ports
+      .injectsbiterr   (),
+      .injectdbiterr   (),
+      .sbiterr         (),
+      .dbiterr         () 
+   );
+
+   /* The size of this fifo has to be large enough to fit the previous modules' headers
+    * and the ethernet header */
+   xpm_fifo_sync #(
+      .FIFO_MEMORY_TYPE     ("auto"),
+      .ECC_MODE             ("no_ecc"),
+      .FIFO_WRITE_DEPTH     (16),
+      .WRITE_DATA_WIDTH     (TUPLE_WIDTH),
+      .WR_DATA_COUNT_WIDTH  (1),
+      //.PROG_FULL_THRESH     (PROG_FULL_THRESH),
+      .FULL_RESET_VALUE     (0),
+      .USE_ADV_FEATURES     ("0707"),
+      .READ_MODE            ("fwft"),
+      .FIFO_READ_LATENCY    (1),
+      .READ_DATA_WIDTH      (TUPLE_WIDTH),
+      .RD_DATA_COUNT_WIDTH  (1),
+      .PROG_EMPTY_THRESH    (10),
+      .DOUT_RESET_VALUE     ("0"),
+      .WAKEUP_TIME          (0)
+   ) tuple_fifo (
+      // Common module ports
+      .sleep           (),
+      .rst             (~axis_resetn),
+
+      // Write Domain ports
+      .wr_clk          (axis_aclk),
+      .wr_en           (tuple_valid),
+      .din             (tuple),
+      .full            (),
+      .prog_full       (tuple_fifo_nearly_full),
+      .wr_data_count   (),
+      .overflow        (),
+      .wr_rst_busy     (),
+      .almost_full     (),
+      .wr_ack          (),
+
+      // Read Domain ports
+      .rd_en           (tuple_fifo_rd_en),
+      .dout            (tuple_fifo_out),
+      .empty           (tuple_fifo_empty),
+      .prog_empty      (),
+      .rd_data_count   (),
+      .underflow       (),
+      .rd_rst_busy     (),
+      .almost_empty    (),
+      .data_valid      (),
+
+      // ECC Related ports
+      .injectsbiterr   (),
+      .injectdbiterr   (),
+      .sbiterr         (),
+      .dbiterr         () 
+   );
+
+   pkt_parser
+     #(.C_S_AXIS_DATA_WIDTH (C_S_AXIS_DATA_WIDTH),
+       .C_M_AXIS_TUSER_WIDTH (C_S_AXIS_TUSER_WIDTH),
+       .TUPLE_WIDTH (TUPLE_WIDTH)
+       ) pkt_parser
+       ( 
+
+      // --- Input
+      .tdata         (s_axis_tdata),
+      .tvalid        (s_axis_tvalid),
+      .tlast         (s_axis_tlast),
+      .tuser         (s_axis_tuser),
+
+      // --- Output
+      .tuple         (tuple),
+      .tuple_valid   (tuple_valid),
+
+      // --- Misc
+      // --- Input
+      .reset         (~axis_resetn),
+      .clk           (axis_aclk)
+    );
+
+
+   always @(*) begin 
+      ptr_pos_next = ptr_pos;
+
+      m_axis_tlast = in_fifo_tlast;
+      m_axis_tuser = in_fifo_tuser;
+      m_axis_tkeep = in_fifo_tkeep; // to set full word length 128{4'hf};
+      m_axis_tdata = in_fifo_tdata;
+      m_axis_tvalid = 0;
+
+      wr_mem_en_next = 1'b0;
+
+      tuple_fifo_rd_en = 0;
+      in_fifo_rd_en = 0;
+      
+      case(state)
+
+         FIRST_BATCH: begin
+            if(!tuple_fifo_empty) begin 
+               if(m_axis_tready) begin
+                  if(in_fifo_tuser[SRC_PORT_POS])
+                     m_axis_tuser[DST_PORT_POS+2] = 1'b1;
+                  else if(in_fifo_tuser[SRC_PORT_POS+2])
+                      m_axis_tuser[DST_PORT_POS] = 1'b1;
+                  m_axis_tvalid = 1;
+                  m_axis_tlast = 0;
+                  m_axis_tkeep = 16{4'hf};
+                  m_axis_tdata = {mem_history[0],mem_history[1],mem_history[2],mem_history[3],ptr_pos,60'b0};
+                  wr_mem_en_next = 1;
+                  state_next = SECOND_BATCH;
+               end
+            end
+         end
+
+         SECOND_BATCH: begin
+            if(m_axis_tready) begin
+               m_axis_tvalid = 1;
+               m_axis_tlast = 0;
+               m_axis_tkeep = 16{4'hf};
+               m_axis_tdata = {mem_history[4],mem_history[5],mem_history[6],mem_history[7],64'b0};
+               tuple_fifo_rd_en = 1;
+               ptr_pos_next = ptr_pos + 1;
+               state_next = THIRD_BATCH;
+         end
+
+         THIRD_BATCH: begin
+            if(m_axis_tready) begin
+               m_axis_tvalid = 1;
+               m_axis_tlast = 0;
+               m_axis_tkeep = 16{4'hf};
+               m_axis_tdata = {mem_history[8],mem_history[9],mem_history[10],mem_history[11],64'b0};
+               state_next = FOURTH_BATCH;
+         end
+
+         FOURTH_BATCH: begin
+            if(m_axis_tready) begin
+               m_axis_tvalid = 1;
+               m_axis_tlast = 0;
+               m_axis_tkeep = 16{4'hf};
+               m_axis_tdata = {mem_history[12],mem_history[13],mem_history[14],mem_history[15],64'b0};
+               state_next = SEND_PACKET;
+         end
+
+         SEND_PACKET: begin
+            if(!in_fifo_empty) begin
+               if(m_axis_tready) begin
+                  m_axis_tvalid = 1;
+                  in_fifo_rd_en = 1;
+                  if(in_fifo_tlast)
+                     state_next = FIRST_BATCH;
+               end
+            end   
+         end               
+
+   always @(posedge clk) begin
+      if(reset) begin
+         state	      <= IDLE;
+         ptr_pos     <= 0;
+         wr_mem_en   <= 0;
+         for(i=0;i<NUM_CORES_HISTORY;i=i+1)
+            mem_history[i] <= 112{1'b0};
+      end
+      else begin
+         state		<= state_next;
+         ptr_pos  <= ptr_pos_next;
+         wr_mem_en<= wr_mem_en_next;
+         if (wr_mem_en)
+            mem_history[ptr_pos] <= tuple;
+      end
+   end
+
+
+   
+endmodule // packet_history
+
---
+++ hw/lib/contrib/packet_history_v1_0_0/hdl/pkt_parser.v
@@ -0,0 +1,124 @@
+/*******************************************************************************
+*
+* Copyright (C) 2010, 2011 The Board of Trustees of The Leland Stanford
+*                          Junior University
+* Copyright (C) 2010, 2011 Muhammad Shahbaz
+* Copyright (C) 2015 Gianni Antichi
+* Copyright (C) 2021 Yuta Tokusashi
+* Copyright (C) 2023 Gianni Antichi
+* All rights reserved.
+*
+* This software was developed by
+* Stanford University and the University of Cambridge Computer Laboratory
+* under National Science Foundation under Grant No. CNS-0855268,
+* the University of Cambridge Computer Laboratory under EPSRC INTERNET Project EP/H040536/1 and
+* by the University of Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-11-C-0249 ("MRC2"), 
+* as part of the DARPA MRC research programme,
+* and by the University of Cambridge Computer Laboratory under EPSRC EARL Project
+* EP/P025374/1 alongside support from Xilinx Inc.
+*
+* @NETFPGA_LICENSE_HEADER_START@
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @NETFPGA_LICENSE_HEADER_END@
+*
+********************************************************************************/
+
+
+  module pkt_parser
+    #(parameter C_S_AXIS_DATA_WIDTH	   = 512,
+      parameter C_S_AXIS_TUSER_WIDTH	= 128,
+      parameter TUPLE_WIDTH            = 112
+      )
+   (// --- Input signals
+    input [C_S_AXIS_DATA_WIDTH-1:0]    tdata,
+    input                              tvalid,
+    input                              tlast,
+    input [C_S_AXIS_TUSER_WIDTH-1:0]   tuser,
+   
+    // --- Output signals
+    output reg [TUPLE_WIDTH-1:0]       tuple, 
+    output reg                         tuple_valid,
+
+    // --- Misc
+    input                              reset,
+    input                              clk
+   );
+
+   //------------------ Internal Parameter ---------------------------
+   //localparam	ETH_ARP	= 16'h0806;	// byte order = Big Endian
+   //localparam	ETH_IP 	= 16'h0800;	// byte order = Big Endian
+   //localparam  DST_MAC_POS = C_S_AXIS_DATA_WIDTH;
+   //localparam  ETHTYPE_POS = C_S_AXIS_DATA_WIDTH - 96; (not used but useful reminder for parsing: taken from the NetFPGA router project)
+
+
+   localparam  IDLE		   = 1;
+   localparam  WAIT_NEXT	= 2;
+
+   localparam  SRC_IP_POS = C_S_AXIS_DATA_WIDTH - 208; // L2 header = 48+48+16; L3 header before IPs is 32+32+32
+   localparam  DST_IP_POS = C_S_AXIS_DATA_WIDTH - 240; // SRC_IP_POS + 32
+   localparam  SRC_L4_PORT_POS = C_S_AXIS_DATA_WIDTH - 272; // DST_IP_POS + 32
+   localparam  DST_L4_PORT_POS = C_S_AXIS_DATA_WIDTH - 288; // SRC_L4_PORT_POS + 16
+
+
+   //---------------------- Wires/Regs -------------------------------
+   reg [TUPLE_WIDTH-1]  tuple_next;
+   reg                  tuple_valid_next;
+
+   reg [1:0]            state, state_next;
+
+   /******************************************************************
+    * Get the L3/L4 destination, source and length of the pkt
+    *****************************************************************/
+   always @(*) begin
+      tuple_valid_next = 0;
+      tuple_next = tuple;
+      state_next = state;
+
+      case(state)
+
+         IDLE: begin
+            if(tvalid) begin
+               tuple_next = {tdata[SRC_IP_POS-1:SRC_IP_POS-32],tdata[DST_IP_POS-1:DST_IP_POS-32],tdata[SRC_L4_PORT_POS-1:SRC_L4_PORT-16],tdata[DST_L4_PORT-1:DST_L4_PORT-16],tuser[15:0]}; //{src_ip,dst_ip,src_l4_port,dst_l4_port,pkt_length}
+               tuple_valid_next = 1;
+               if(!tlast)
+                  state_next	= WAIT_NEXT;
+            end
+         end
+
+         WAIT_NEXT: begin
+            if(tlast) begin
+               state_next = IDLE;
+            end
+         end
+
+      endcase // case(state)
+   end // always @(*)
+
+   always @(posedge clk) begin
+      if(reset) begin
+         state	      <= IDLE;
+         tuple	      <= 0;
+         tuple_valid <= 0;
+      end
+      else begin
+         state		   <= state_next;
+         tuple	      <= tuple_next;
+         tuple_valid <= tuple_valid_next;
+      end
+   end
+
+endmodule // pkt_parser
+
+
